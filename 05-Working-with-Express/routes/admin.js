08156190162f46713a0abbd922c400fb03c37a16
raziullah-khann <raziullahkhan25@gmail.com>
2024-07-17 14:01:26 +0530

    Converting .html file into .pug and Adding layout for common code

--- 05-Working-with-Express/routes/admin.js
+++ 05-Working-with-Express/routes/admin.js
@@ -8,7 +8,8 @@ const products = [];
 // /admin/add-product => GET
 router.get('/add-product',  (req, res, next)=>{
     // res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
 });
 
 // /admin/product => POST
