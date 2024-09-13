02d94db07b1ba730d4e6f27863af3467531d8aba
raziullah-khann <raziullahkhan25@gmail.com>
2024-09-13 23:47:22 +0530

    Adding the Request Driven Login Solution

--- 05-Working-with-Express/controllers/error.js
+++ 05-Working-with-Express/controllers/error.js
@@ -1,5 +1,5 @@
 exports.get404Page = (req, res, next) => {
     // res.status(404).send('<h1>Page not found</h1>');
     // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', { pageTitle: 'Page not found', path: '' });
+    res.status(404).render('404', { pageTitle: 'Page not found', path: '', isAuthenticated: req.isLoggedIn });
 }
