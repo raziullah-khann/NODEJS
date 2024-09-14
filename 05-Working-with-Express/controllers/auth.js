028d8ca6396c671eb8dd3cd2658a84194f8be946
raziullah-khann <raziullahkhan25@gmail.com>
2024-09-14 17:17:40 +0530

    Setting a Cookie

--- 05-Working-with-Express/controllers/auth.js
+++ 05-Working-with-Express/controllers/auth.js
@@ -1,13 +1,15 @@
 exports.getLogin = (req, res, next) => {
+   const isLoggedIn = req.get("Cookie").split("=")[1];
+    
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: req.isLoggedIn
+    isAuthenticated: isLoggedIn
   });
 };
 
 //here i will get my login data
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true; 
+    res.setHeader("Set-Cookie", "loggedIn=true");
     res.redirect("/")
   };
