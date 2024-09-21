7f2f9f2576875a70773a1763ea1c1e08dda5a9a2
raziullah-khann <raziullahkhan25@gmail.com>
2024-09-21 14:57:07 +0530

    Encrypting Password using bcryptjs

--- 05-Working-with-Express/controllers/auth.js
+++ 05-Working-with-Express/controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
@@ -8,16 +9,16 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
-//here i will set my user data and isLoggedIn 
+//here i will set my user data and isLoggedIn
 exports.postLogin = (req, res, next) => {
   User.findById("66dc6a8ca269a15d09fda59b") // Assuming you're retrieving the same user
     .then((user) => {
-      req.session.isLoggedIn = true;  //This line sets a isLoggedIn property in the session object to true. This indicates that the user is now authenticated and logged in.
+      req.session.isLoggedIn = true; //This line sets a isLoggedIn property in the session object to true. This indicates that the user is now authenticated and logged in.
       req.session.user = user; //The entire user object is stored in the session.
-      req.session.save((err)=> {
+      req.session.save((err) => {
         console.log(err);
         res.redirect("/"); //here you are done saving the session data then it will redirect
-      })
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -30,27 +31,33 @@ exports.getSignup = (req, res, next) => {
     pageTitle: "Sign Up",
     isAuthenticated: false,
   });
-}
+};
 
 exports.postSignup = (req, res, next) => {
-  const {email, password, confirmPassword} = req.body;
-  //one step we want to do before we create a new user,  first we check user is already exist or in my database bcs i don't want duplicate email
-  User.findOne({email: email}).then(userDoc => {
-    if(userDoc){
-      return res.redirect("/signup");
-    }
-    const user = new User({
-      email: email,
-      password: password,
-      cart: { items: []},
+  const { email, password, confirmPassword } = req.body;
+  //one step we want to do before we create a new user, first we check user is already exist or not in my database bcs i don't want duplicate email
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        return res.redirect("/signup");
+      }
+      return bcrypt.hash(password, 12);
+    })
+    .then((hashPassword) => {
+      const user = new User({
+        email: email,
+        password: hashPassword,
+        cart: { items: [] },
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
+    })
+    .catch((err) => {
+      console.log(err);
     });
-    return user.save();
-  }).then(result => {
-    res.redirect("/login");
-  }).catch(err => {
-    console.log(err);
-  });
-}
+};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
