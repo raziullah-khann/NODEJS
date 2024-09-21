2da964b43e5accc8caa00adba0c2f41a7afe1304
raziullah-khann <raziullahkhan25@gmail.com>
2024-09-21 21:16:42 +0530

    Adding the signup functionality using bcrypt.compare()

--- 05-Working-with-Express/controllers/auth.js
+++ 05-Working-with-Express/controllers/auth.js
@@ -11,17 +11,30 @@ exports.getLogin = (req, res, next) => {
 
 //here i will set my user data and isLoggedIn
 exports.postLogin = (req, res, next) => {
-  User.findById("66dc6a8ca269a15d09fda59b") // Assuming you're retrieving the same user
+  const { email, password } = req.body;
+  User.findOne({ email: email }) // Assuming you're retrieving the same user
     .then((user) => {
-      req.session.isLoggedIn = true; //This line sets a isLoggedIn property in the session object to true. This indicates that the user is now authenticated and logged in.
-      req.session.user = user; //The entire user object is stored in the session.
-      req.session.save((err) => {
-        console.log(err);
-        res.redirect("/"); //here you are done saving the session data then it will redirect
-      });
+      if (!user) { //findOne() => if user entered wrong email return null 
+        return res.redirect("/login");
+      }
+      bcrypt
+        .compare(password, user.password) //this compare return promise taht resolves to true, if password match
+        .then((doMatch) => {
+          if (doMatch) {
+            req.session.isLoggedIn = true; //This line sets a isLoggedIn property in the session object to true. This indicates that the user is now authenticated and logged in.
+            req.session.user = user; //The entire user object is stored in the session.
+            return req.session.save((err) => {
+              console.log(err);
+              res.redirect("/"); //here you are done saving the session data then it will redirect
+            });
+          }else{
+            res.redirect("/login");
+          }
+        })
     })
-    .catch((err) => {
+    .catch((err) => { // Executes only if there is an error (e.g., database or bcrypt error).
       console.log(err);
+      res.redirect("/login");
     });
 };
 
@@ -41,18 +54,19 @@ exports.postSignup = (req, res, next) => {
       if (userDoc) {
         return res.redirect("/signup");
       }
-      return bcrypt.hash(password, 12);
-    })
-    .then((hashPassword) => {
-      const user = new User({
-        email: email,
-        password: hashPassword,
-        cart: { items: [] },
-      });
-      return user.save();
-    })
-    .then((result) => {
-      res.redirect("/login");
+      return bcrypt
+        .hash(password, 12)
+        .then((hashPassword) => {
+          const user = new User({
+            email: email,
+            password: hashPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect("/login");
+        });
     })
     .catch((err) => {
       console.log(err);
