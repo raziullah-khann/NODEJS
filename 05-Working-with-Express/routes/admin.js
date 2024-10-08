0cb02fcd7718eea56577c3eebba7bdd8ae71f378
raziullah-khann <raziullahkhan25@gmail.com>
2024-10-08 23:36:37 +0530

    Using Middleware to protected Routes with the help of session

--- 05-Working-with-Express/routes/admin.js
+++ 05-Working-with-Express/routes/admin.js
@@ -2,25 +2,26 @@ const path = require('path'); //it provide the utilities for working with file a
 const express = require('express');
 const rootDir = require('../util/path');
 const adminControllers = require('../controllers/admin');
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router(); //This Router is like a mini express app tied to the other express app or pluggable into other express app.
 
 
 // /admin/add-product => GET
-router.get('/add-product',  adminControllers.getAddProductPage);
+router.get('/add-product', isAuth, adminControllers.getAddProductPage);
 
 // /admin/products => GET, fetch all product from product model with edit and delete button
-router.get('/products', adminControllers.getProducts);
+router.get('/products', isAuth, adminControllers.getProducts);
 
 // /admin/product => POST,  to get the req.body of add-product body
-router.post('/add-product', adminControllers.postAddProductPage);
+router.post('/add-product', isAuth, adminControllers.postAddProductPage);
 
-router.get('/edit-product/:productId', adminControllers.getEditProduct);
+router.get('/edit-product/:productId', isAuth, adminControllers.getEditProduct);
 
 // /admin/edit-product => POST,  to get the req.body of edit-product body
-router.post('/edit-product', adminControllers.postEditProduct);
+router.post('/edit-product', isAuth, adminControllers.postEditProduct);
 
-router.post('/delete-product', adminControllers.postDeleteProduct);
+router.post('/delete-product', isAuth, adminControllers.postDeleteProduct);
 
 // exports.routes = router;
 // exports.products = products;
--- 05-Working-with-Express/routes/shop.js
+++ 05-Working-with-Express/routes/shop.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path"); //it provide the utilities for working with file and directory paths.
 const shopControllers = require('../controllers/shop');
+const isAuth = require('../middleware/is-auth');
 
 //create router object
 const router = express.Router(); //this is mini express app tied to the other express app
@@ -15,18 +16,18 @@ router.get("/products", shopControllers.getProducts);
 router.get("/products/:productId", shopControllers.getProduct);
 
 //get acart page
-router.get("/cart", shopControllers.getCart);
+router.get("/cart", isAuth, shopControllers.getCart);
 
 //user click add to cart button then send product id on server
-router.post("/cart", shopControllers.postCart);
+router.post("/cart", isAuth, shopControllers.postCart);
 
 //if user click on delete button on cart page then that product deleted
-router.post("/cart-delete-item", shopControllers.postCartDeleteProduct);
+router.post("/cart-delete-item", isAuth, shopControllers.postCartDeleteProduct);
 
 //if we click on OrderNow button in cart then execute this middleware function
-router.post("/create-order", shopControllers.postOrder);
+router.post("/create-order", isAuth, shopControllers.postOrder);
 
 //if user get the orders path then it will be execute
-router.get("/orders", shopControllers.getOrders);
+router.get("/orders", isAuth, shopControllers.getOrders);
 
 module.exports = router;
