a0d26595a95bb87a44434dd92ef2e72695ca4f58
raziullah-khann <raziullahkhan25@gmail.com>
2024-11-19 00:57:29 +0530

    Setup and Basic validation

--- 05-Working-with-Express/routes/auth.js
+++ 05-Working-with-Express/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authControllers = require("../controllers/auth");
+const { check } = require("express-validator");
 const router = express.Router(); //this is mini express app tied to the other express app
 
 router.get("/login", authControllers.getLogin);
@@ -8,7 +9,7 @@ router.get("/signup", authControllers.getSignup);
 
 router.post("/login", authControllers.postLogin);
 
-router.post("/signup", authControllers.postSignup);
+router.post("/signup",check("email").isEmail(), authControllers.postSignup);
 
 router.post("/logout", authControllers.postLogout);
 
