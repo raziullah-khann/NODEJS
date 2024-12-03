98cbd6e7b4087fb9cdde3b0d998d813e8eea7d73
raziullah-khann <raziullahkhan25@gmail.com>
2024-12-03 14:21:39 +0530

    Serving images statically

--- 05-Working-with-Express/app.js
+++ 05-Working-with-Express/app.js
@@ -53,6 +53,7 @@ const pageNotFound = require("./controllers/error");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single("image"));
 app.use(express.static(path.join(__dirname, "public"))); //__dirname is a global variable in Node.js that represents the current directory path of the current module
+app.use(express.static(path.join(__dirname, "images")))
 
 app.use(
   session({
